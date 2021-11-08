f44f1a2292d7d379f73103246a487b8a36d66b44
joliennoe <jolien_noe@hotmail.be>
2021-11-08 15:13:29 +0100

    Basis les over variabelen en if/else

---
+++ variabele.py
@@ -0,0 +1,20 @@
+#if 1 + 1 == 2: # eerst if, dan de voorwaarde met 2 == gevolgd door een dubbel punt
+#    print("hallo") # zorg ervoor dat er 1 tab of 4 spaties volgen op de lijn na de if lijn
+#hier kan elif, else of niets komen 
+
+# mogelijke checks op een vergelijking
+#  == wil zeggen is gelijk aan
+# < > <= >=   kleiner dan, groter dan, kleiner of gelijk, groter of gelijk
+
+lijst = [1,2,3,"nick",5,"jolien"]
+if 4 in lijst:
+    print("het zit erin")
+else:
+    print("het zit er niet in")
+
+a = knuffel
+b = mij
+c= alstubliefd
+
+if a + b + c =
+ print ("ik wil knuffelen")
